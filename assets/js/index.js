792495495186597f710a213a5dae8ae34fedead2
optimist19 <oluwasegunbamidele3@gmail.com>
2023-04-13 13:49:18 +0100

    better code

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,7 +11,7 @@ if(navToggle){
 
 
 if(navClose){
-	navClose.addEventListener('click', ()=>{
+	navClose.addEventListener('click', () =>{
 		navMenu.classList.remove('show-menu')
 	})
 }
@@ -30,7 +30,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 
-const skillsContent = document.getElementsByClassName('skills__content')
+const skillsContent = document.getElementsByClassName('skills__content'),
 skillsHeader = document.querySelectorAll(".skills__header")
 
 function toggleSkills(){
@@ -47,4 +47,24 @@ function toggleSkills(){
 
 skillsHeader.forEach((el) =>{
 	el.addEventListener('click', toggleSkills)
+})
+
+
+const tabs = document.querySelectorAll('[data-target]'),
+tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+	tab.addEventListener('click', () =>{
+		const target = document.querySelector(tab.dataset.target)
+
+		tabContents.forEach(tabContent =>{
+			tabContent.classList.remove('qualification__active')
+		})
+		target.classList.add('qualification__active')
+
+		tabs.forEach(tab =>{
+			tab.classList.remove('qualification__active')
+		})
+		tab.classList.add('qualification__active')
+	})
 })
