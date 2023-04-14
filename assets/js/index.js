0eba2656f744194b7932f709a64a4cb63f2f1afb
optimist19 <oluwasegunbamidele3@gmail.com>
2023-04-14 11:35:06 +0100

    testimonial done

--- assets/js/index.js
+++ assets/js/index.js
@@ -99,7 +99,7 @@ modalCloses.forEach((modalClose) =>{
 
 // portfolio swiper
 
-let swiper = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
 	cssMode: true,
 	loop: true,
 
@@ -113,4 +113,24 @@ let swiper = new Swiper('.portfolio__container', {
 	},
 	// mousewheel: true,
 	// keyboard: true,
+})
+
+// Testimonial
+
+let swiperTestimonial = new Swiper('.testimonial__container', {
+	loop: true,
+	grabCursor: true,
+	spaceBetween: 48,
+
+	
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		dynamicBullets: true,
+	},
+	breakpoints:{
+		568:{
+			slidesPerView: 2,
+		}
+	}
 })
