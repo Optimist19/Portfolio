8b9b7fba0e7b9dbc11d7cd0bf94de0b9f7550bac
optimist19 <oluwasegunbamidele3@gmail.com>
2023-05-04 13:07:02 +0100

    well done job

--- assets/js/index.js
+++ assets/js/index.js
@@ -89,36 +89,6 @@ window.addEventListener('load', function(){
 
 
 
-
-
-
-// services modal
-
-// const modalViews = document.querySelectorAll('.services__modal'),
-// modalBtns = document.querySelectorAll('.services__button'),
-// modalCloses = document.querySelectorAll('.services__modal-close')
-
-
-// let modal = function(modalClick){
-// 	modalViews[modalClick].classList.add('active-modal')
-// }
-
-// modalBtns.forEach((modalBtn, i) =>{
-// 	modalBtn.addEventListener('click', () =>{
-// 		modal(i)
-// 	})
-// })
-
-
-// modalCloses.forEach((modalClose) =>{
-// 	modalClose.addEventListener('click', () =>{
-// 		modalViews.forEach((modalView) =>{
-// 			modalView.classList.remove('active-modal')
-// 		})
-// 	})
-// })
-
-
 // portfolio swiper
 
 let swiperPortfolio = new Swiper('.portfolio__container', {
